addfc60cf1b9580b969d77cee9cd9c6166df8ec9
aylinkaymazm <aylinkaymazm@gmail.com>
2021-01-04 03:05:33 +0300

    admin category update

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -75,9 +75,12 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function edit(Category $category)
+    public function edit(Category $category,$id)
     {
-        //
+        $data = Category::find($id);
+        $datalist = DB::table( 'categories')->get()->where('parent_id',0);
+
+        return view('admin.category_edit',['data' => $data,'datalist' =>$datalist]);
     }
 
     /**
@@ -87,9 +90,17 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, Category $category,$id)
     {
-        //
+        $data = Category::find($id);
+        $data->parent_id =  $request->input('parent_id');
+        $data->title=  $request->input('title');
+        $data->keywords=  $request->input('keywords');
+        $data->description=  $request->input('description');
+        $data->slug= $request->input('slug');
+        $data->status=  $request->input('status');
+        $data->save();
+        return redirect()->route('admin_category');
     }
 
     /**
--- resources/views/admin/category_edit.blade.php
+++ resources/views/admin/category_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 
-@section('title', 'Admin Panel add Page')
+@section('title', 'Admin Panel Edit Page')
 
 @section('content')
     <!-- page content -->
@@ -8,7 +8,7 @@
         <div class="">
             <div class="page-title">
                 <div class="title_left">
-                    <h3>Category Add Form</h3>
+                    <h3>Category Edit Form</h3>
                 </div>
 
                 <div class="title_right">
@@ -28,22 +28,7 @@
             <div class="row">
                 <div class="col-md-12 col-sm-12  ">
                     <div class="x_panel">
-                        <div class="x_title">
-                            <ul class="nav navbar-right panel_toolbox">
-                                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                        <a class="dropdown-item" href="#">Settings 1</a>
-                                        <a class="dropdown-item" href="#">Settings 2</a>
-                                    </div>
-                                </li>
-                                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                                </li>
-                            </ul>
-                            <div class="clearfix"></div>
-                        </div>
+
                         <div class="x_content">
 
                             <div class="col-md-6 ">
@@ -66,15 +51,15 @@
                                     </div>
                                     <div class="x_content" style="display: block;">
                                         <br>
-                                        <form role="form" action="{{route('admin_category_create')}}" method="post" class="form-horizontal form-label-left">
+                                        <form role="form" action="{{route('admin_category_update',['id'=>$data->id])}}" method="post" class="form-horizontal form-label-left">
                                             @csrf
                                             <div class="form-group row ">
                                                 <label class="control-label col-md-3 col-sm-3 ">Parent</label>
 
                                                 <select class="form-control" name="parent_id">
-                                                    <option selected="selected">Main Category</option>,
+                                                    <option value="0">Main Category</option>
                                                     @foreach ( $datalist as $rs )
-                                                        <option value="{{$rs->id}}">{{$rs->title}}</option>
+                                                        <option value="{{$rs->id}}" @if ($rs->id == $data->parent_id) selected="selected" @endif >{{$rs->title}}</option>
                                                     @endforeach
                                                 </select>
                                             </div>
@@ -82,28 +67,28 @@
                                             <div class="form-group row ">
                                                 <label class="control-label col-md-3 col-sm-3 ">Title</label>
                                                 <div class="col-md-9 col-sm-9 ">
-                                                    <input type="text" class="form-control" name="title">
+                                                    <input type="text" id="title" class="form-control" value="{{$data->title}}" name="title">
                                                 </div>
                                             </div>
 
                                             <div class="form-group row ">
                                                 <label class="control-label col-md-3 col-sm-3 ">Keywords</label>
                                                 <div class="col-md-9 col-sm-9 ">
-                                                    <input type="text" class="form-control" name="keywords">
+                                                    <input type="text" class="form-control" value="{{$data->keywords}}" name="keywords">
                                                 </div>
                                             </div>
 
                                             <div class="form-group row ">
                                                 <label class="control-label col-md-3 col-sm-3 ">Description</label>
                                                 <div class="col-md-9 col-sm-9 ">
-                                                    <input type="text" class="form-control" name="description">
+                                                    <input type="text" class="form-control" value="{{$data->description}}" name="description">
                                                 </div>
                                             </div>
 
                                             <div class="form-group row ">
                                                 <label class="control-label col-md-3 col-sm-3 ">Slug</label>
                                                 <div class="col-md-9 col-sm-9 ">
-                                                    <input type="text" class="form-control" name="slug">
+                                                    <input type="text" class="form-control" value="{{$data->slug}}" name="slug">
                                                 </div>
                                             </div>
 
@@ -111,7 +96,8 @@
                                                 <label class="control-label col-md-3 col-sm-3 ">Status</label>
                                                 <div class="col-md-9 col-sm-9 ">
                                                     <select class="form-control" name="status">
-                                                        <option selected="selected">False</option>
+                                                        <option selected="selected">{{$data->status}}</option>
+                                                        <option>False</option>
                                                         <option>True</option>
                                                     </select>
                                                 </div>
@@ -119,7 +105,7 @@
                                             <div class="ln_solid"></div>
                                             <div class="form-group">
                                                 <div class="col-md-9 col-sm-9  offset-md-3">
-                                                    <button type="submit" class="btn btn-success">Add Category</button>
+                                                    <button type="submit" class="btn btn-success">Update Category</button>
                                                 </div>
                                             </div>
 
@@ -127,8 +113,6 @@
                                     </div>
                                 </div>
                             </div>
-
-
                         </div>
                     </div>
                 </div>
