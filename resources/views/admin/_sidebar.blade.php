b817c425924182bb5899e74b8222c3c6305b2c12
aylinkaymazm <aylinkaymazm@gmail.com>
2021-01-05 04:15:28 +0300

    reservation ekleniyor işlem 3

--- resources/views/admin/_sidebar.blade.php
+++ resources/views/admin/_sidebar.blade.php
@@ -4,55 +4,23 @@
     <div class="menu_section">
         <h3>General</h3>
         <ul class="nav side-menu">
-            <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="index.html">Dashboard</a></li>
-                    <li><a href="index2.html">Dashboard2</a></li>
-                    <li><a href="index3.html">Dashboard3</a></li>
-                </ul>
-            </li>
-            <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="form.html">General Form</a></li>
-                    <li><a href="form_advanced.html">Advanced Components</a></li>
-                    <li><a href="form_validation.html">Form Validation</a></li>
-                    <li><a href="form_wizards.html">Form Wizard</a></li>
-                    <li><a href="form_upload.html">Form Upload</a></li>
-                    <li><a href="form_buttons.html">Form Buttons</a></li>
-                </ul>
-            </li>
-            <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="general_elements.html">General Elements</a></li>
-                    <li><a href="media_gallery.html">Media Gallery</a></li>
-                    <li><a href="typography.html">Typography</a></li>
-                    <li><a href="icons.html">Icons</a></li>
-                    <li><a href="glyphicons.html">Glyphicons</a></li>
-                    <li><a href="widgets.html">Widgets</a></li>
-                    <li><a href="invoice.html">Invoice</a></li>
-                    <li><a href="inbox.html">Inbox</a></li>
-                    <li><a href="calendar.html">Calendar</a></li>
-                </ul>
-            </li>
-            <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
+            <li><a href="{{route('admin_home')}}"><i class="fa fa-home"></i> Home </a></li>
+            <li><a href="{{route('admin_category')}}" class="nav-link"><i class="fa fa-edit"></i> Category </a></li>
+            <li><a href="{{route('admin_products')}}" class="nav-link"><i class="fa fa-edit"></i> Product </a></li>
+
+            <li><a><i class="fa fa-table"></i> Reservation <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
                     <li><a href="tables.html">Tables</a></li>
                     <li><a href="tables_dynamic.html">Table Dynamic</a></li>
                 </ul>
             </li>
-            <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
+            <li><a><i class="fa fa-bar-chart-o"></i> Level 1<span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
                     <li><a href="chartjs.html">Chart JS</a></li>
                     <li><a href="chartjs2.html">Chart JS2</a></li>
                     <li><a href="morisjs.html">Moris JS</a></li>
                     <li><a href="echarts.html">ECharts</a></li>
                     <li><a href="other_charts.html">Other Charts</a></li>
-                </ul>
-            </li>
-            <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-                    <li><a href="fixed_footer.html">Fixed Footer</a></li>
                 </ul>
             </li>
         </ul>
@@ -114,9 +82,6 @@
     <a data-toggle="tooltip" data-placement="top" title="Lock">
         <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
     </a>
-    <a data-toggle="tooltip" data-placement="top" title="Logout" href="{{route('admin_logout')}}>
-        <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-    </a>
 </div>
 <!-- /menu footer buttons -->
 </div>
@@ -133,6 +98,7 @@
                 <ul class=" navbar-right">
                     <li class="nav-item dropdown open" style="padding-left: 15px;">
                         <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+
                             <img src="{{ asset('assets')}}/admin/production/images/img.jpg" alt="" class="d-block">{{Auth::user()->name}} </a>
                         <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
 
