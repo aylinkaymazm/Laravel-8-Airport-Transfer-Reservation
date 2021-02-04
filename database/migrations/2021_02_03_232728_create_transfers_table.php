4af810c8d6710e2f1c95ce4b146a1f47774a36e8
aylinkaymazm <aylinkaymazm@gmail.com>
2021-02-04 23:24:25 +0300

    Transfer added

--- database/migrations/2021_02_03_232728_create_transfers_table.php
+++ database/migrations/2021_02_03_232728_create_transfers_table.php
@@ -15,18 +15,20 @@ class CreateTransfersTable extends Migration
     {
         Schema::create('transfers', function (Blueprint $table) {
             $table->id()->autoIncrement();
-
-            $table->integer('rzervation_id');
-            $table->integer('image_id');
-            $table->string('title')->nullable();
-            $table->string('keywords')->nullable();
-            $table->string('description')->nullable();
-            $table->string('image',75)->nullable();
-            $table->integer('category_id');
-            $table->text('detail')->nullable();
-            $table->integer('capasity',20);
-            $table->integer('baseprice',20);
-            $table->integer('kmprice',20);
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->string('name',20);
+            $table->string('email',50);
+            $table->string('phone',20);
+            $table->string('from_destination',150);
+            $table->string('to_destination',150);
+            $table->string('date',44);
+            $table->string('time',44);
+            $table->integer('person_number');
+            $table->float('total');
+            $table->integer('price');
+            $table->string('note',50);
+            $table->string('IP',20);
             $table->string('status',30)->default('New');
             $table->timestamps();
         });
